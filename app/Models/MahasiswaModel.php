9e56c915043aba5a66914bdaa0c4bef5cb5ea8e5
mahesadarmasatria <mahesadarmasatria@gmail.com>
2023-08-13 13:16:56 +0700

    update

---
+++ app/Models/MahasiswaModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class MahasiswaModel extends Model
+{
+    protected $table = "mahasiswa";
+    protected $primaryKey = "id_mahasiswa";
+    protected $allowedFields = ["nama_mahasiswa","email_mahasiswa", "no_hp"];
+    protected $useTimestamps = false;
+
+
+}
