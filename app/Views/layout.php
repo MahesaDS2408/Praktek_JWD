84d09acc9f2506c579bf3cb7f88da865d7cd498b
mahesadarmasatria <mahesadarmasatria@gmail.com>
2023-08-14 09:59:17 +0700

    Update layout.php

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Dashboard <?= ucwords($this->session->get('level')) ?> - Karang Taruna</title>
+        <title>Dashboard - Praktek JWD</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/litepicker/dist/css/litepicker.css" rel="stylesheet" />
         <link href="<?php base_url() ?>/assets/admin/css/styles.css" rel="stylesheet" />
